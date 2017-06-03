fb9ea8a6a948181ec64e311fd170661e3c9fbd9e
marcos10soares <marcossoares100@gmail.com>
2017-06-03 12:22:14 +0100

    Add files via upload

---
+++ v1.0/config.py
@@ -0,0 +1,20 @@
+#Your ethereum wallet to watch
+address = "0x6B298eAaB45794A427376F56AD199f00FF3a87d0"
+
+#Your workers name
+workers = {'WorkerName1', 'WorkerName2', 'WorkerName3'}
+
+#Gmail Account email:
+smtpUser = 'youremail@gmail.com'
+
+#Gmail Account password:
+smtpPass = 'YourPa$$word'
+
+#To Email - to receive notification
+toEmail = 'destinationemail@gmail.com'
+
+#Subject Text
+subjectText = 'Workers are down: '
+
+#Email Body Text
+body = "Time is money!."
