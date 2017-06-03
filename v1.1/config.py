1f03ffdee21b71104d1cd15cde83557a45ad1971
marcos10soares <marcossoares100@gmail.com>
2017-06-03 12:20:18 +0100

    Add files via upload

---
+++ v1.1/config.py
@@ -0,0 +1,33 @@
+#Your ethereum wallet to watch
+address = "0x6B298eAaB45794A427376F56AD199f00FF3a87d0"
+
+#Your workers name
+workers = {'WorkerName1', 'WorkerName2', 'WorkerName3'}
+
+#Gmail Account email:
+smtpUser = 'youremail@gmail.com'
+
+#Gmail Account password:
+smtpPass = 'YourPa$$word'
+
+#To Email - to receive notification
+toEmail = 'destinationemail@gmail.com'
+
+#Subject Text
+subjectText = 'Workers are down: '
+
+#Email Body Text
+body = "Time is money!."
+
+
+### Configuration for push notifications with Pushover Service
+
+#Your Pushover token
+pushoverToken = 'Your-Pushover-Application-Token'
+
+#Your userkey - device you want to target
+pushoverUserKey = 'Your-Pushover-User-Key'
+
+notificationMessage = 'Miners stopped: '
+
+notificationTitle = 'Workers offline'
